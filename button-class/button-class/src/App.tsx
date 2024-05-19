ba3c6f6d75da8d724bf86ca2a32d614c20d9d8b7
zelyankovas <vas.zelenko@gmail.com>
2024-05-19 11:31:15 +0300

    change button class

--- button-class/button-class/src/App.tsx
+++ button-class/button-class/src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, {MouseEventHandler} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "./components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const myFirstSubscriber: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    //     console.log("Hello I'm Vasya!")
+    // }
+    // const mySecondSubscriber = () => {
+    //     console.log("Hello I'm Ivan!")
+    // }
+    // const onClickHandler = (name: string) => {
+    //     console.log(name)
+    // }
+
+    return (
+        <div className="App">
+            {/*<button onClick={(event) => {console.log('Hello')}}>MyYouTubeChannel-1</button>*/}
+            {/*<button onClick={(event) => onClickHandler('Vasya')}>MyYouTubeChannel-1</button>*/}
+            {/*<button onClick={(event) => onClickHandler('Ivan')}>MyYouTubeChannel-2</button>*/}
+            {/*<button onClick={() => onClickHandler('Some info')}>MyYouTubeChannel-3</button>*/}
+            {/*<button onClick={mySecondSubscriber}>MyYouTubeChannel-3</button>*/}
+
+            {/*<button>MyYouTubeChannel-1</button>*/}
+            {/*<button>MyYouTubeChannel-2</button>*/}
+            <Button name={"MyYouTubeChannel-1"}/>
+            <Button name={"MyYouTubeChannel-2"}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ button-class/button-class/src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type ButtonType = {
+    name: string
+}
+export const Button = (props:ButtonType) => {
+    return (
+        <button>{props.name}</button>
+    );
+};
+
