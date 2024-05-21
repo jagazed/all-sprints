63a3c24b42886cc53bbe274963b28dbe311b430f
zelyankovas <vas.zelenko@gmail.com>
2024-05-21 18:32:29 +0300

    add tests01

---
+++ tests01/src/01-hello-tests/01.ts
@@ -0,0 +1,17 @@
+
+export function sum(a: number, b: number) {
+    return a + b;
+}
+
+export function mult(a: number, b: number) {
+    return a * b;
+}
+
+export function splitIntoWords(sentense: string) {
+    return sentense.toLowerCase()
+        .split(" ")
+        .filter( w => w !=="" && w !== "-")
+        .map( w => w.replace("!","")
+            .replace(".", "")
+            .replace(",", ""));
+}
