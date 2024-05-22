c98b2e1a6b4d6b409dc865bf2babd1815412624e
zelyankovas <vas.zelenko@gmail.com>
2024-05-22 16:01:14 +0300

    add tests03

--- tests01/src/03/03.ts
+++ tests01/src/03/03.ts
@@ -1,10 +1,8 @@
-import {student, StudentType} from "../01-hello-tests/02/02";
+import {StudentType} from "../01-hello-tests/02/02";
 
 export const sum = (a: number, b: number) => {
     return a+b;
 }
-
-const res = sum(2,3)
 
 export const addSkills = ( student: StudentType, skill: string) => {
     student.technologies.push({
