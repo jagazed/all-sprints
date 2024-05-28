2fbc0b98f532fd0f3535f7b6519d46f970354e02
zelyankovas <vas.zelenko@gmail.com>
2024-05-28 16:14:18 +0300

    change tests02

--- tests01/src/01-hello-tests/02/02.test.ts
+++ tests01/src/01-hello-tests/02/02.test.ts
@@ -7,22 +7,36 @@ beforeEach(()=> {
         title: "New York",
         houses: [
             {
+                id: 1,
                 buildedAt: 2012,
                 repaired: false,
                 address: {number: 100, street: {title: "White street"}}
             },
             {
+                id: 2,
                 buildedAt: 2008,
                 repaired: false,
                 address: {number: 100, street: {title: "Happy street"}}
             },
             {
+                id: 3,
                 buildedAt: 2020,
                 repaired: false,
                 address: {number: 101, street: {title: "Happy street"}}
             }
         ],
-        governmentBuildings: [],
+        governmentBuildings: [
+            {
+                type: "HOSPITAL",
+                address: { street: {title: "Central Str"}, number: 12},
+                budget: 200000, staffCount: 200
+            },
+            {
+                type: "FIRE-STATION",
+                address: { street: {title: "South Str"}, number: 12},
+                budget: 500000, staffCount: 1000
+            }
+        ],
         citizensNumber: 1000000
     }
 })
--- tests01/src/01-hello-tests/02/02.ts
+++ tests01/src/01-hello-tests/02/02.ts
@@ -8,6 +8,27 @@ type AddressType ={
     city: LocalCityType
 }
 
+export type HouseType = {
+    id?: number
+    buildedAt: number
+    repaired: boolean
+    address: AddressType
+}
+
+export type GovernmentBuildingType = {
+    type: "HOSPITAL" | "FIRE-STATION"
+    budget: number
+    staffCount: number
+    address: AddressType
+}
+
+export type CityType = {
+    title: string
+    houses: Array<HouseType>
+    governmentBuildings : Array<GovernmentBuildingType>
+    citizensNumber: number
+}
+
 type TechType = {
     id: number
     title: string
