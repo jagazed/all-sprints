81fcd8a3f057017b6947d812522b9395433c0922
zelyankovas <vas.zelenko@gmail.com>
2024-05-28 15:57:19 +0300

    add tests04

---
+++ tests01/src/04/04.test.ts
@@ -0,0 +1,38 @@
+test("should take old men older then 90", ()=>{
+    const ages = [10, 20, 22, 1, 100, 90, 14];
+
+    const oldAges = ages.filter(age => age > 90);
+
+    expect(oldAges.length).toBe(1);
+    expect(oldAges[0]).toBe(100);
+})
+
+test("should take courses chipper 160", ()=>{
+    const courses = [
+        {title: "CSS", price: 110},
+        {title: "JS", price: 200},
+        {title: "REACT", price: 150}
+    ]
+
+
+    const cheapCourses = courses.filter(courses => courses.price < 160);
+
+    expect(cheapCourses.length).toBe(2);
+    expect(cheapCourses[0].title).toBe("CSS");
+    expect(cheapCourses[1].title).toBe("REACT");
+})
+
+test("get only completed tasks", () => {
+    const tasks = [
+        {id: 1, title: "Bread", isDone: false},
+        {id: 2, title: "Milk", isDone: true},
+        {id: 3, title: "Salt", isDone: false},
+        {id: 4, title: "Sugar", isDone: true},
+    ]
+
+    const completedTasks = tasks.filter(task => task.isDone)
+    expect(completedTasks.length).toBe(2);
+    expect(completedTasks[0].id).toBe(2);
+    expect(completedTasks[1].id).toBe(4);
+
+})
---
+++ tests01/src/04/04.ts
@@ -0,0 +1,29 @@
+const ages = [10, 20, 22, 1, 100, 90, 14];
+
+const predicate = (age: number) => {
+    return age > 90;
+}
+
+console.log(predicate(ages));
+const oldAges = [100]; // > 90
+
+type CoursesType = {
+    title: string
+    price: number
+}
+
+const courses = [
+    {title: "CSS", price: 110},
+    {title: "JS", price: 200},
+    {title: "REACT", price: 150}
+]
+
+
+const cheapPredicate = (courses: CoursesType) => {
+    return courses.price < 160;
+}
+
+const cheapCourses = [
+    {title: "CSS", price: 110},
+    {title: "REACT", price: 150}
+]
