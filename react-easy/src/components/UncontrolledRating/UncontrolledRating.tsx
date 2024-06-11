15bb96babd92960ff0298cba09da5d88b216984b
zelyankovas <vas.zelenko@gmail.com>
2024-06-11 20:23:40 +0300

    change react-easy curse

--- react-easy/src/components/UncontrolledRating/UncontrolledRating.tsx
+++ react-easy/src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -20,9 +20,6 @@ type StarPropsType = {
 }
 function Star(props: StarPropsType) {
     console.log("Star rendering");
-    // if (props.selected === true){
-    //     return <span><b>star</b> </span>
-    // } else return <span>star </span>
 
     return props.selected ? <span><b>star</b> </span> : <span>star </span>;
 }
