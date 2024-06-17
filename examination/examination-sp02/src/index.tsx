4ab91bb94c83a5711292a43eeea56475e75c8776
zelyankovas <vas.zelenko@gmail.com>
2024-06-17 09:24:53 +0300

    change total exam

--- examination/examination-sp02/src/index.tsx
+++ examination/examination-sp02/src/index.tsx
@@ -1,47 +1,23 @@
-import React, {useState, MouseEvent, ChangeEvent} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-function Notes() {
-    const [newNote, setNewNote] = useState<string>("")
-    const [notes, setNotes] = useState<Array<string>>([])
-    const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement> )=>
-        setNewNote(e.currentTarget.value)
-    const addNote = () => {
-        setNotes([newNote, ...notes])
-        setNewNote("")
-    }
-    return (
-        <div>
-            <textarea
-                value={newNote}
-                onChange={onChangeHandler}
-                onBlur={addNote}
-            />
-            <div>
-                <button
-                    onClick={() => {setNotes([])}}
-                >Clear notes list</button>
-            </div>
-            <h4>Notes:</h4>
-            <div>
-                {notes.map(n => <p>{n}</p>)}
-            </div>
-        </div>
-    )
+type UserWalletType = {
+    title: string
+    amount: number
+}
+type UserWalletPropsType = {
+    wallet: UserWalletType
 }
 
-ReactDOM.render(
-    <Notes/>, document.getElementById('root')
-);
-// Что надо написать вместо ххх,
-// чтобы при клике список заметок очищался?
+export const UserWallet: React.FC<UserWalletPropsType> = (props) => {
+    return <div>title: {props.wallet.title}, amount: {props.wallet.amount}</div>
+}
 
+export const UserMoney = () => {
+    const wallets = [
+        {title: 'bitcoin', amount: 1},
+        {title: '$', amount: 100}
+    ]
 
-
-
-
-
-
-
-
+    return <div>
+        <UserWallet wallet={wallets[0]} />
+        <UserWallet wallet={wallets[1]} />
+    </div>
+}
