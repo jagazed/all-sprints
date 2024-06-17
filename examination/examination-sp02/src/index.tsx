5819dc94e75d947ae81dfb3b4b38f67d627fc133
zelyankovas <vas.zelenko@gmail.com>
2024-06-17 09:41:37 +0300

    change total exam

--- examination/examination-sp02/src/index.tsx
+++ examination/examination-sp02/src/index.tsx
@@ -1,12 +1,40 @@
-type UserPropsType = {
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+type UserType = {
+    id: number
     name: string
-    description: string
-}
-export const User: React.FC<UserPropsType> = (props) => {
-    return <div>
-        <h1>Имя: {props.name}</h1>
-        <div>Описание: {props.description}</div>
-    </div>
+    age: number
 }
 
-//Что нужно написать вместо ххх, что бы код работал?
+function User(props: UserType) {
+    return (
+        <li>User {props.name}: {props.age} y.o.</li>
+    )
+}
+
+function UsersList() {
+    const data: Array<UserType> = [
+        {id: 1, name: "Bob", age: 25},
+        {id: 2, name: "Alex", age: 28},
+        {id: 3, name: "Ann", age: 23},
+        {id: 4, name: "John", age: 30},
+    ]
+    const [users, setUsers] = useState<Array<UserType>>(data)
+    // Пользователи старше 25 лет:
+    const olderThen25Users = users.filter(u => u.age > 25)
+    return (
+        <main>
+            <h4>User list:</h4>
+            <ul>
+                { olderThen25Users.map(u => <User key={u.id} {...u}/>)}
+            </ul>
+        </main>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
+// Что надо написать вместо xxx, чтобы код работал?
