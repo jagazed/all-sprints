1de986968c483c0377793c4c249e6e3dca63a1f8
zelyankovas <vas.zelenko@gmail.com>
2024-06-17 09:42:17 +0300

    change total exam

--- examination/examination-sp02/src/index.tsx
+++ examination/examination-sp02/src/index.tsx
@@ -1,40 +1,21 @@
-import React, {useState} from 'react';
+import React, {useState, MouseEvent, ChangeEvent} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-type UserType = {
-    id: number
-    name: string
-    age: number
-}
-
-function User(props: UserType) {
+function User() {
+    const [userName, setUserName] = useState<string>("")
     return (
-        <li>User {props.name}: {props.age} y.o.</li>
-    )
-}
-
-function UsersList() {
-    const data: Array<UserType> = [
-        {id: 1, name: "Bob", age: 25},
-        {id: 2, name: "Alex", age: 28},
-        {id: 3, name: "Ann", age: 23},
-        {id: 4, name: "John", age: 30},
-    ]
-    const [users, setUsers] = useState<Array<UserType>>(data)
-    // Пользователи старше 25 лет:
-    const olderThen25Users = users.filter(u => u.age > 25)
-    return (
-        <main>
-            <h4>User list:</h4>
-            <ul>
-                { olderThen25Users.map(u => <User key={u.id} {...u}/>)}
-            </ul>
-        </main>
+        <div>
+            <p>{userName}</p>
+            <input
+                value={userName}
+                onChange={(e) => setUserName(e.currentTarget.value)}
+            />
+        </div>
     )
 }
 
 ReactDOM.render(
-    <UsersList/>, document.getElementById('root')
+    <User/>, document.getElementById('root')
 );
-// Что надо написать вместо xxx, чтобы код работал?
+// Что надо написать вместо ххх, чтобы инпут был контролируемым?
