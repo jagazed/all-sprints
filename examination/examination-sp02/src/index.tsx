8528a3cbcb59d9d096798b9d9dabecb3a733c879
zelyankovas <vas.zelenko@gmail.com>
2024-06-17 09:10:17 +0300

    change exam4

--- examination/examination-sp02/src/index.tsx
+++ examination/examination-sp02/src/index.tsx
@@ -1,34 +1,38 @@
-import React, {useState, MouseEvent, ChangeEvent} from 'react';
+import React, {useState, MouseEvent} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Notes() {
-    const [newNote, setNewNote] = useState<string>("")
-    const [notes, setNotes] = useState<Array<string>>([])
-    const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement> )=>
-        setNewNote(e.currentTarget.value)
-    const addNote = () => {
-        setNotes([newNote, ...notes])
-        setNewNote("")
+function AuthForm() {
+    const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        alert()
     }
     return (
-        <div>
-            <textarea
-                value={newNote}
-                onChange={onChangeHandler}
-                onBlur = {addNote}
-            />
-            <h4>Notes:</h4>
+        <form>
             <div>
-                {notes.map((n,i )=> <p key={i}>{n}</p>)}
+                <label style={{padding: "10px 0"}}>
+                    Name:
+                    <input type={"email"} name={"email"}/>
+                </label>
             </div>
-        </div>
+            <div>
+                <label style={{padding: "10px 0"}}>
+                    Password:
+                    <input type={"password"} name={"password"}/>
+                </label>
+            </div>
+            <button
+                onClick={onClickHandler}
+                type={"submit"}>
+                Log in
+            </button>
+        </form>
     )
 }
 
-
 ReactDOM.render(
-    <Notes/>, document.getElementById('root')
+    <AuthForm/>, document.getElementById('root')
 );
-// Что надо написать вместо ххх,
-// чтобы при потере инпутом фокуса добавлялась заметка?
+// Что надо написать вместо ххх, чтобы данные из формы
+// не отправлялись на сервер и страница не перезагружалась
+// при клике по кнопке?
