7795d980dc4a57b62c14b30211aab0dc860bc7a2
zelyankovas <vas.zelenko@gmail.com>
2024-06-17 09:28:01 +0300

    change total exam

--- examination/examination-sp02/src/index.tsx
+++ examination/examination-sp02/src/index.tsx
@@ -1,23 +1,46 @@
-type UserWalletType = {
-    title: string
-    amount: number
-}
-type UserWalletPropsType = {
-    wallet: UserWalletType
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+type UserType = {
+    id: number
+    name: string
+    age: number
 }
 
-export const UserWallet: React.FC<UserWalletPropsType> = (props) => {
-    return <div>title: {props.wallet.title}, amount: {props.wallet.amount}</div>
+function User(props: UserType) {
+    return (
+        <li>User {props.name}: {props.age} y.o.</li>
+    )
 }
 
-export const UserMoney = () => {
-    const wallets = [
-        {title: 'bitcoin', amount: 1},
-        {title: '$', amount: 100}
+function UsersList() {
+    const state = [
+        {id: 1, name: "Bob", age: 34},
+        {id: 2, name: "Alex", age: 25},
+        {id: 3, name: "Ann", age: 30},
+        {id: 4, name: "John", age: 23},
+    ]
+    const users = [
+        {id: 1, userName: "Bob", age: 34},
+        {id: 2, userName: "Alex", age: 25},
+        {id: 3, userName: "Ann", age: 30},
+        {id: 4, userName: "John", age: 23},
     ]
 
-    return <div>
-        <UserWallet wallet={wallets[0]} />
-        <UserWallet wallet={wallets[1]} />
-    </div>
+    const [usersList, setUsersList] = useState<Array<UserType>>(users)
+    return (
+        <main>
+            <h5>User list:</h5>
+            <p>Тут будет список пользователей</p>
+        </main>
+    )
 }
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
+// Что надо написать вместо XXX, чтобы код работал?
+// ❗ Если мы отмапим массив, то должны увидеть данные пользователей
+// ❗ Ответ дать минимально возможным объёмом кода
+
