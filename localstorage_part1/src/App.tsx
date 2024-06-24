0c0f13ccdcfb9c53093899832d0a1133c499a305
zelyankovas <vas.zelenko@gmail.com>
2024-06-24 19:42:26 +0300

    add localstorage

--- localstorage_part1/src/App.tsx
+++ localstorage_part1/src/App.tsx
@@ -4,32 +4,38 @@ import './App.css';
 
 function App() {
 
-  const [value, setValue] = useState(0)
+    const [value, setValue] = useState(0)
 
-  const incHandler = () => {
-    setValue(value +1)
-  }
+    const incHandler = () => {
+        setValue(value + 1)
+    }
 
-  const setToLocalStorageHandler = () => {
-    localStorage.setItem('counterValue', JSON.stringify(value))
-  }
-  const getFromLocalStorageHandler = () => {
-    let valueAsString = localStorage.getItem('counterValue')
-      if (valueAsString) {
-          let newValue = JSON.parse(valueAsString)
-          setValue(newValue)
-      }
+    const setToLocalStorageHandler = () => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+    }
+    const getFromLocalStorageHandler = () => {
+        let valueAsString = localStorage.getItem('counterValue')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setValue(newValue)
+        }
 
-  }
+    }
 
-  return (
-      <div className="App">
-        <h1>{value}</h1>
-        <button onClick={incHandler}>inc</button>
-        <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
-        <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
-      </div>
-  );
+    const clearLocalStorage = () => {
+        localStorage.clear()
+        setValue(0)
+    }
+
+    return (
+        <div className="App">
+            <h1>{value}</h1>
+            <button onClick={incHandler}>inc</button>
+            <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+            <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
+            <button onClick={clearLocalStorage}>clearLocalStorage</button>
+        </div>
+    );
 }
 
 export default App;
