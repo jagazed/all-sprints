4735128966d4c076e9632402de19885104ff0a40
zelyankovas <vas.zelenko@gmail.com>
2024-07-01 23:31:11 +0300

    add exam sp02

--- examination/examination-sp01/src/index.tsx
+++ examination/examination-sp01/src/index.tsx
@@ -2,26 +2,30 @@ type StudentType = {
     id: number
     name: string
 }
+
 type FriendsType = {
-    [key: string]: Array<String>
+    [key: string]: Array<string>
 }
+
 export const students: Array<StudentType> = [
     {id: 1, name: "Bob"},
     {id: 2, name: "Alex"},
     {id: 3, name: "Ann"},
     {id: 4, name: "Charley"},
 ]
+
 export const friends: FriendsType = {
     1: ["Oliver", "Jack", "Oscar",],
     2: ["Jack", "Lewis", "Thomas",],
     3: ["William", "Michael", "Lewis",],
     4: ["Oscar", "James", "William",],
 }
-console.log("result",friends[students[0].id][3])
 
 //Дан список студентов и структура,
 //которая содержит список друзей каждого из студентов.
 //Id студента является ключом к массиву его друзей.
+//Какое значение лежит тут: friends[students[1].id][2]?
 //Какое значение лежит тут: friends[students[0].id][3]?
-
-
+//Какое значение лежит тут:  friends[3][1]?
+console.log("res1", friends[students[1].id][2])
+console.log(friends[3][1])
