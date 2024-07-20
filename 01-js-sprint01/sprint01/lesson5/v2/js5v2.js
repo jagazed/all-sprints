01b88928a6f9eb96d063497446378e6b76ec86b9
zelyankovas <vas.zelenko@gmail.com>
2024-07-20 11:28:34 +0300

    add hosting

---
+++ 01-js-sprint01/sprint01/lesson5/v2/js5v2.js
@@ -0,0 +1,44 @@
+
+// это все что связано с хостингом(всплытием)
+
+// let car = 'bmw'
+//
+// function startEngine() {
+//     console.log(`start ${car}`);
+//
+// }
+//
+// car = 'audi';
+// startEngine();
+
+// const glovalLE = {
+//     environmentRecords: {
+//
+//     },
+//
+//     outer: null
+// }
+//
+// globalLE {} -> null
+
+foo();
+// bar();
+// baz();
+console.log(a)
+
+function foo() {
+    console.log("foo");
+}
+
+const bar = function () {
+    console.log("bar")
+}
+
+const baz = () => {
+    console.log("baz")
+}
+
+var a = 'str'
+let b = 10;
+const c = 20;
+console.log(a)
