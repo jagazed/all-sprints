0d54067814434a8237d0431698bb1f3c55db9e85
zelyankovas <vas.zelenko@gmail.com>
2024-08-01 12:30:44 +0300

    change exam 2 sp3

--- examination/examination-sp02/src/index.tsx
+++ examination/examination-sp02/src/index.tsx
@@ -1,19 +1,75 @@
-import {combineReducers, createStore} from 'redux'
+import {createStore} from 'redux'
+import ReactDOM from 'react-dom'
+import {Provider, useSelector, useDispatch} from 'react-redux'
+import React from 'react'
 
-let initialState = {items: [{name: 'Dimych'}, {name: 'Ignat'}]}
-const usersReducer = (state = initialState, action: any) => {
+const students = {
+    students: [
+        {id: 1, name: 'Bob'},
+        {id: 2, name: 'Alex'},
+        {id: 3, name: 'Donald'},
+        {id: 4, name: 'Ann'},
+    ]
+}
+type RemoveStudentAT = {
+    type: "REMOVE-STUDENT"
+    id: number
+}
+const RemoveStudentAC = (id: number): RemoveStudentAT => ({
+    type: "REMOVE-STUDENT",
+    id
+})
+
+const studentsReducer = (state = students, action: RemoveStudentAT) => {
+    switch (action.type) {
+        case "REMOVE-STUDENT":
+            return {
+                ...state,
+                students: state.students.filter(s => s.id !== action.id)
+            }
+    }
     return state
 }
 
-const store = createStore(combineReducers({
-    users: usersReducer
-}))
+const store = createStore(studentsReducer)
+type RootStateType = ReturnType<typeof studentsReducer>
 
-store.subscribe(() => {
-    const state = store.getState()
-    console.log(state)
-})
 
-store.dispatch({type: 'ANY'})
+const StudentList = () => {
+    const listItemStyles = {
+        width: "100px",
+        borderBottom: "1px solid gray",
+        cursor: "pointer",
+    }
+    const students = useSelector((state: RootStateType) => state.students)
+    const dispatch = useDispatch()
+    const studentsList = students.map(s => {
+        const removeStudent = () => {
+            dispatch(RemoveStudentAC(s.id))
+        }
+        return (
+            <li key={s.id}
+                style={listItemStyles}
+                onClick={removeStudent}>
+                {s.name}
+            </li>)
+    })
+    return (
+        <ol>
+            {studentsList}
+        </ol>
 
-//Что нужно написать вместо XXX, чтобы получить актуальный стейт?
+    )
+}
+
+
+ReactDOM.render(<div>
+        <Provider store={store}>
+            <StudentList/>
+        </Provider>
+    </div>,
+    document.getElementById('root')
+)
+
+// Что нужно написать вместо XXX, YYY и ZZZ, чтобы при клике по имени студент
+// удалялся из списка? Напишите через пробел.
