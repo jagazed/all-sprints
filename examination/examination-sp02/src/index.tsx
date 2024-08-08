ab1e43086d5a8ea7a6788c790fa6a3303cc42f26
zelyankovas <vas.zelenko@gmail.com>
2024-08-08 16:23:35 +0300

    change exam 3 sp3

--- examination/examination-sp02/src/index.tsx
+++ examination/examination-sp02/src/index.tsx
@@ -1,4 +1,4 @@
-import React, {memo, useCallback, useEffect, useMemo, useState} from 'react'
+import React, {useCallback, useState} from 'react'
 import ReactDOM from 'react-dom'
 
 export const App = () => {
