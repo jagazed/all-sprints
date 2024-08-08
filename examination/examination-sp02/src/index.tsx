0eedc8daf945bd0b87f5db4915c8315d359a688f
zelyankovas <vas.zelenko@gmail.com>
2024-08-08 16:41:12 +0300

    change exam 3 sp3

--- examination/examination-sp02/src/index.tsx
+++ examination/examination-sp02/src/index.tsx
@@ -1,45 +1,48 @@
-import React, {useCallback, useState} from 'react'
+import React, { useCallback, useState } from 'react'
 import ReactDOM from 'react-dom'
 
 export const App = () => {
-    const [temp, setTemp] = useState(10)
-    const [seconds, setSeconds] = useState(100)
+    const [temp, setTemp] = useState(100)
+    const [seconds, setSeconds] = useState(0)
 
-    const increaseSeconds = () => setSeconds(seconds + 10)
-    const increaseTemp = useCallback(() =>  setTemp(temp + 1), [temp])
+    const resetTemp = useCallback(() => setTemp(0), [])
 
-
+    const incSec = useCallback(() => setSeconds(seconds + 1), [seconds])
 
     return <>
-        <TempDisplay temp={temp} increaseTemp={increaseTemp}/>
-
-        <div>
-            <b>Секунды :</b> {seconds} с
-            <button style={{marginLeft: '15px'}}
-                    onClick={increaseSeconds}>
-                Увеличить на 10 секунд
-            </button>
-        </div>
+        <TempDisplay temp={temp} resetTemp={resetTemp}/>
+        <SecDisplay seconds={seconds} incSec={incSec}/>
     </>
 }
 const TempDisplay = React.memo((props: any) => {
     console.log('Render TempDisplay')
     return (
-        <div style={{marginBottom: '15px'}}
-             onClick={props.reset}>
-            <b>Температура:</b> {props.temp} &#176;
-            <button style={{marginLeft: '15px'}}
-                    onClick={props.increaseTemp}>
-                Увеличить температуру на 1 градус
+        <div style={{marginBottom: '10px'}} onClick={props.reset}>
+            <p>
+                <b>Температура: </b>{props.temp} &#176;
+            </p>
+            <button onClick={props.resetTemp}>Сбросить температуру к 0</button>
+        </div>
+    )
+})
+
+const SecDisplay = React.memo((props: any) => {
+    console.log('Render SecDisplay')
+    return (
+        <div>
+            <p><b>Секунды:</b> {props.seconds} c </p>
+            <button style={{marginRight: '20px'}}
+                    onClick={props.incSec}>
+                Увеличить время на 1 секунду
             </button>
         </div>
     )
 })
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'))
 
-// Что надо написать вместо XXX для того, чтобы обязательно выполнялись 2 условия:
-// 1) При нажатии на кнопку "Увеличить температуру на 1 градус" температура увеличивалась
-// 2) Компонент TempDisplay не должен перерисовываться при нажатии на кнопку "Увеличить на 10 секунд"
+// Почему не корректно работает счетчик времени при нажатии на кнопку (срабатывает только 1 раз) ?
+// Найдите в чем причина.
+// Исправленную версию строки напишите в качестве ответа
 
-// Пример ответа: useEffect(() => setCounter(count + 1), [count])
+// Пример ответа: const incSec = () => setSeconds(seconds + 1)
