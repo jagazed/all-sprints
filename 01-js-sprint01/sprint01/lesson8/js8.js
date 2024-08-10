236b4430c86d4d25238a4cd7617862d8c41d3684
zelyankovas <vas.zelenko@gmail.com>
2024-08-10 21:22:47 +0300

    change lesson8

---
+++ 01-js-sprint01/sprint01/lesson8/js8.js
@@ -0,0 +1,23 @@
+// eventloop
+
+//1
+// const a = () => {}
+//
+// const b = () => {
+//     a()
+// }
+//
+// const c = () => {
+//     b()
+// }
+// c()
+
+//2
+// console.log('start')
+// setTimeout(() => {
+//     console.log('timeout1')
+// }, 1000)
+// setTimeout(() => {
+//     console.log('timeout2')
+// }, 0)
+// console.log('end')
