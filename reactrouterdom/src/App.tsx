aac028bfd3c81f02b53b5be6ddcd46a98c3cbdb5
zelyankovas <vas.zelenko@gmail.com>
2024-08-17 02:17:51 +0300

    add react router dom

--- reactrouterdom/src/App.tsx
+++ reactrouterdom/src/App.tsx
@@ -1,17 +1,39 @@
 import React, {useState} from 'react';
 import styles from "./components/Site.module.css";
+import {PageOne} from "./components/pages/PageOne";
+import {PageTwo} from "./components/pages/PageTwo";
+import {PageThree} from "./components/pages/PageThree";
+import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
+import {Error404} from "./components/pages/Error404";
+import {S} from "./components/pages/_styles"
 
-
+const PATH = {
+    PAGE1: '/page1',
+    PAGE2: '/page2',
+    PAGE3: '/page3',
+    ERROR: '/error404'
+} as const
 function App() {
     return (
         <div>
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    Здесь будет навигация
+                    <S.NavWrapper><NavLink to={PATH.PAGE1} >Page1</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE2} >Page2</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE3} >Page3</NavLink></S.NavWrapper>
                 </div>
                 <div className={styles.content}>
-                    Здесь будут кроссовки
+                    <Routes>
+                        <Route path="/" element={<Navigate to={"/page1"} />} />
+
+                        <Route path={PATH.PAGE1} element={<PageOne />} />
+                        <Route path={PATH.PAGE2} element={<PageTwo />} />
+                        <Route path={PATH.PAGE3} element={<PageThree />} />
+
+                        <Route path={PATH.ERROR} element={<Error404 />} />
+                        <Route path="/*" element={<Navigate to={PATH.ERROR} />} />
+                    </Routes>
                 </div>
             </div>
             <div className={styles.footer}>abibas 2023</div>
@@ -21,3 +43,5 @@ function App() {
 
 
 export default App;
+
+
