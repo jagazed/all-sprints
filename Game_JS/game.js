94d69267425d47569877bebff5dc200922abc01d
zelyankovas <vas.zelenko@gmail.com>
2024-10-05 10:48:30 +0300

    add gm v1

--- Game_JS/game.js
+++ Game_JS/game.js
@@ -7,11 +7,17 @@ export class Game {
             rowsCount: 3,
             columnsCount: 3
         },
-        jumpInterval: 1000 // milliseconds
+        jumpInterval: 1000, // milliseconds,
+        winScore: 20
     }
 
     #numberUtility;
     #googlePosition;
+    #player1Position;
+    #player2Position;
+    #player1Score = 0;
+    #player2Score = 0;
+    #googleScore = 0;
 
 
     // dependency injection
@@ -22,10 +28,14 @@ export class Game {
     }
 
     async setSettings(settings) {
+        if (settings.gridSize.rowsCount * settings.gridSize.columnsCount < 4) {
+            throw new Error('Grid must have at least 4 cells')
+        }
         this.#settings = settings
     }
 
     async #jumpGoogle() {
+        // ироки должны быть уже рапосложены на игровом поле
         // create new position
         const newPosition = new Position(
             await this.#numberUtility
@@ -35,12 +45,59 @@ export class Game {
         )
 
         // check new posision
-        if (!!this.#googlePosition && newPosition.isEqual(this.#googlePosition)) {
+        // if ( ( !!this.#googlePosition && newPosition.isEqual(this.#googlePosition) )
+        //     || newPosition.isEqual(this.#player1Position))  {
+        //     return this.#jumpGoogle();
+        // }
+        // мой код
+        if ((!!this.#googlePosition && newPosition.isEqual(this.#googlePosition))
+            || newPosition.isEqual(this.#player1Position) || newPosition.isEqual(this.#player2Position)) {
             return this.#jumpGoogle();
         }
 
         this.#googlePosition = newPosition
+
+        const catcher = this.#checkGoogleCatching()
+        if (catcher) {
+            if (catcher === "Player 1") {
+                this.#player1Score++
+            } else if (catcher === "Player 2") {
+                this.#player2Score++
+            }
+        } else {
+            this.#googleScore++
+        }
+
+        this.#checkGameEnd()
     }
+
+    async #initPlayer1Position() {
+        // google must be jump after this initialization
+        // create new position
+        const newPosition = new Position(
+            await this.#numberUtility
+                .getRandomNumber(0, this.#settings.gridSize.columnsCount - 1),
+            await this.#numberUtility
+                .getRandomNumber(0, this.#settings.gridSize.rowsCount - 1)
+        )
+
+        this.#player1Position = newPosition
+    }
+
+    //мой код
+    async #initPlayer2Position() {
+        const newPosition = new Position(
+            await this.#numberUtility
+                .getRandomNumber(0, this.#settings.gridSize.columnsCount - 1),
+            await this.#numberUtility
+                .getRandomNumber(0, this.#settings.gridSize.rowsCount - 1)
+        )
+        if (this.#player1Position && newPosition.isEqual(this.#player1Position)) {
+            return this.#initPlayer2Position();
+        }
+        this.#player2Position = newPosition
+    }
+
     async #runGoogleJumpInterval() {
         setInterval(async () => {
             await this.#jumpGoogle();
@@ -49,7 +106,9 @@ export class Game {
 
     async start() {
         // "Принцип единого уровня абстракции" (Single Level of Abstraction Principle, SLAP).
-        this.#status = 'IN-PROGRESS'
+        this.#status = GAME_STATUSES.IN_PROGRESS
+        await this.#initPlayer1Position();
+        await this.#initPlayer2Position();
         await this.#jumpGoogle();
         await this.#runGoogleJumpInterval();
     }
@@ -62,10 +121,160 @@ export class Game {
     async getGooglePosition() {
         return this.#googlePosition
     }
+
+    async getPlayer1Position() {
+        return this.#player1Position
+    }
+    async getPlayer2Position() {
+        return this.#player2Position
+    }
+    async getCheckGoogleCatching() {
+        return this.#checkGoogleCatching()
+    }
+    async getJumpGoogle() {
+        return this.#jumpGoogle()
+    }
+
+    async movePlayer1(direction) {
+        const delta = {
+            x: 0, y: 0
+        }
+
+        switch (direction) {
+            case MOVE_DIRECTIONS.UP:
+                delta.y = -1
+                break
+            case MOVE_DIRECTIONS.DOWN:
+                delta.y = 1
+                break
+            case MOVE_DIRECTIONS.LEFT:
+                delta.x = -1
+                break
+            case MOVE_DIRECTIONS.RIGHT:
+                delta.x = 1
+                break
+            default:
+                throw new Error('Invalid direction')
+        }
+        let newPosition;
+
+
+        try {
+            newPosition = new Position(
+                this.#player1Position.x + delta.x,
+                this.#player1Position.y + delta.y
+            )
+        } catch (e) {
+            return
+        }
+
+        if (!this.#isPositionWithinGrid(newPosition)) {
+            return;
+        }
+        if (this.#isPositionBusy(newPosition)) {
+            return;
+        }
+
+        const catcher = this.#checkGoogleCatching()
+        if (catcher === "Player 1") {
+            this.#player1Score++
+        }
+
+        this.#player1Position = newPosition;
+
+        this.#checkGameEnd()
+    }
+
+    //мой код
+    async movePlayer2(direction) {
+        const delta = {
+            x: 0, y: 0
+        }
+
+        switch (direction) {
+            case MOVE_DIRECTIONS.UP:
+                delta.y = -1
+                break
+            case MOVE_DIRECTIONS.DOWN:
+                delta.y = 1
+                break
+            case MOVE_DIRECTIONS.LEFT:
+                delta.x = -1
+                break
+            case MOVE_DIRECTIONS.RIGHT:
+                delta.x = 1
+                break
+            default:
+                throw new Error('Invalid direction')
+        }
+        let newPosition;
+
+
+        try {
+            newPosition = new Position(
+                this.#player2Position.x + delta.x,
+                this.#player2Position.y + delta.y
+            )
+        } catch (e) {
+            return
+        }
+
+        if (!this.#isPositionWithinGrid(newPosition)) {
+            return;
+        }
+        if (this.#isPositionBusy(newPosition)) {
+            return;
+        }
+
+        const catcher = this.#checkGoogleCatching()
+        if (catcher === "Player 2") {
+            this.#player2Score++
+        }
+
+        this.#player2Position = newPosition;
+
+        this.#checkGameEnd()
+    }
+
+
+    #isPositionWithinGrid(position) {
+        return (
+            position.x >= 0 && position.x < this.#settings.gridSize.columnsCount
+            && position.y >= 0 && position.y < this.#settings.gridSize.rowsCount
+        )
+    }
+
+    #isPositionBusy(position) {
+        return position.isEqual(this.#player1Position) || position.isEqual(this.#player2Position)
+    }
+
+    #checkGoogleCatching() {
+        if (this.#player1Position && this.#googlePosition && this.#player1Position.isEqual(this.#googlePosition)) {
+            return "Player 1"
+        }
+        if (this.#player2Position && this.#googlePosition && this.#player2Position.isEqual(this.#googlePosition)) {
+            return "Player 2"
+        }
+        return null
+    }
+
+    #checkGameEnd() {
+        if (this.#player1Score >= this.#settings.winScore || this.#player2Score >= this.#settings.winScore || this.#googleScore >= this.#settings.winScore) {
+            this.#status = GAME_STATUSES.FINISHED;
+            clearInterval(this.#runGoogleJumpInterval);
+        }
+    }
 }
 
 export const GAME_STATUSES = {
     PENDING: 'PENDING',
-    IN_PROGRESS: 'IN_PROGRESS',
+    IN_PROGRESS: 'IN-PROGRESS',
     FINISHED: 'FINISHED'
+}
+
+export const MOVE_DIRECTIONS = {
+    UP: 'UP',
+    DOWN: 'DOWN',
+    LEFT: 'LEFT',
+    RIGHT: 'RIGHT'
 }
--- Game_JS/game.test.js
+++ Game_JS/game.test.js
@@ -1,5 +1,7 @@
-import {Game} from "./game.js";
+import {Game, GAME_STATUSES, MOVE_DIRECTIONS} from "./game.js";
 import {NumberUtility} from "./number-utility.js";
+import {Position} from "./position.js";
+import expect from "expect";
 
 
 expect.extend({
@@ -29,6 +31,7 @@ describe("Game", () => {
         const numberUtility = new NumberUtility()
         //const numberUtility = new NumberAPIUtility()
         game = new Game(numberUtility);
+
     }
 
     beforeEach(async () => {
@@ -36,13 +39,13 @@ describe("Game", () => {
     })
 
     it("should return Pending status as inital", async () => {
-        let status  = await game.getStatus()
+        let status = await game.getStatus()
         expect(status).toBe("PENDING")
     })
 
     it("should return In-progress status after start()", async () => {
         await game.start()
-        let status  = await game.getStatus()
+        let status = await game.getStatus()
         expect(status).toBe("IN-PROGRESS")
     })
 
@@ -54,8 +57,8 @@ describe("Game", () => {
             }
         })
         await game.start()
-        let googlePosition  = await game.getGooglePosition()
-        let googlePosition2  = await game.getGooglePosition()
+        let googlePosition = await game.getGooglePosition()
+        let googlePosition2 = await game.getGooglePosition()
 
         expect(googlePosition).toBeEqualPosition(googlePosition2)
 
@@ -67,23 +70,394 @@ describe("Game", () => {
     })
 
     it("google should have random correct positions after jump interval", async () => {
-        for (let i=0;i<10;i++) {
+        for (let i = 0; i < 10; i++) {
             createGame()
             await game.setSettings({
                 gridSize: {
                     rowsCount: 1,
-                    columnsCount: 2 // x
+                    columnsCount: 4 // x
                 },
-                jumpInterval: 100 // 3 seconds
+                jumpInterval: 10 // 3 seconds
             })
             await game.start()
-            let googlePosition  = await game.getGooglePosition()
-
-            await delay(200)
-            let googlePosition2  = await game.getGooglePosition()
-            expect(googlePosition).not.toBeEqualPosition(googlePosition2)
+            let googlePosition = await game.getGooglePosition()
+            await delay(10)
+            let googlePosition2 = await game.getGooglePosition()
+            expect(googlePosition2).not.toBeEqualPosition(googlePosition)
         }
     })
+
+    it("player1 should have random correct positions inside grid after start", async () => {
+        await game.setSettings({
+            gridSize: {
+                rowsCount: 3,
+                columnsCount: 4 // x
+            }
+        })
+        await game.start()
+        let player1Position = await game.getPlayer1Position()
+        expect(player1Position.x).toBeGreaterThanOrEqual(0)
+        expect(player1Position.x).toBeLessThanOrEqual(3)
+
+        expect(player1Position.y).toBeGreaterThanOrEqual(0)
+        expect(player1Position.y).toBeLessThanOrEqual(2)
+    })
+
+    it("player1 should have random correct position not crossed with google after start", async () => {
+        for (let i = 0; i < 40; i++) {
+            createGame()
+
+            await game.setSettings({
+                gridSize: {
+                    rowsCount: 4,
+                    columnsCount: 1 // x
+                }
+            })
+            await game.start()
+            let googlePosition = await game.getGooglePosition()
+            let player1Position = await game.getPlayer1Position()
+
+            expect(player1Position).not.toBeEqualPosition(googlePosition)
+        }
+    })
+
+    it("moving of player1 is correct", async () => {
+        class MockFakeNumberUtility extends NumberUtility {
+            // мвесто рандомной генераици мы хотим подсовывать свои
+            // не рандомные фейки
+            #returnsNumbers = [
+                /*player1*/ 2, 2,
+                /*player2*/ 2, 1,
+                /*google*/ 0, 2
+            ]
+            #callsCount = 0;
+            getRandomNumber() {
+                const returnValue = this.#returnsNumbers[this.#callsCount]
+                this.#callsCount++;
+                return returnValue;
+            }
+        }
+
+        const numberUtil = new MockFakeNumberUtility()
+        const game = new Game(numberUtil);
+
+        await game.setSettings({
+            gridSize: {
+                rowsCount: 3,
+                columnsCount: 3 // x
+            }
+        })
+
+        await game.start()
+        let position1 = await game.getPlayer1Position()
+        let position2 = await game.getPlayer2Position()
+        let googlePosition = await game.getGooglePosition()
+        expect(position1).toBeEqualPosition(new Position(2,2))
+        expect(position2).toBeEqualPosition(new Position(2,1))
+        //expect(position2).toBeEqualPosition(new Position(2,1))
+        expect(googlePosition).toBeEqualPosition(new Position(0,2))
+        // [  ][  ][  ]
+        // [  ][  ][p2]
+        // [ g][  ][p1]
+        // ===========
+        await game.movePlayer1(MOVE_DIRECTIONS.DOWN)
+        position1 = await game.getPlayer1Position()
+        expect(position1).toBeEqualPosition(new Position(2, 2));
+        await game.movePlayer2(MOVE_DIRECTIONS.RIGHT)
+        position2 = await game.getPlayer2Position()
+        expect(position2).toBeEqualPosition(new Position(2, 1));
+        // [  ][  ][  ]
+        // [  ][  ][p2]
+        // [ g][  ][p1]
+        // ===========
+        await game.movePlayer1(MOVE_DIRECTIONS.RIGHT)
+        position1 = await game.getPlayer1Position()
+        expect(position1).toBeEqualPosition(new Position(2, 2))
+        await game.movePlayer2(MOVE_DIRECTIONS.UP)
+        position2 = await game.getPlayer2Position()
+        expect(position2).toBeEqualPosition(new Position(2, 0));
+        // [  ][  ][p2]
+        // [  ][  ][  ]
+        // [ g][  ][p1]
+        // ===========
+        await game.movePlayer1(MOVE_DIRECTIONS.UP)
+        position1 = await game.getPlayer1Position()
+        expect(position1).toBeEqualPosition(new Position(2, 1))
+        await game.movePlayer2(MOVE_DIRECTIONS.UP)
+        position2 = await game.getPlayer2Position()
+        expect(position2).toBeEqualPosition(new Position(2, 0));
+        // [  ][  ][p2]
+        // [  ][  ][p1]
+        // [ g][  ][  ]
+        // ===========
+        await game.movePlayer1(MOVE_DIRECTIONS.LEFT)
+        position1 = await game.getPlayer1Position()
+        expect(position1).toBeEqualPosition(new Position(1, 1))
+        await game.movePlayer2(MOVE_DIRECTIONS.LEFT)
+        position2 = await game.getPlayer2Position()
+        expect(position2).toBeEqualPosition(new Position(1, 0));
+        // [  ][p2][  ]
+        // [  ][p1][  ]
+        // [ g][  ][  ]
+        // ===========
+        await game.movePlayer1(MOVE_DIRECTIONS.RIGHT)
+        position1 = await game.getPlayer1Position()
+        expect(position1).toBeEqualPosition(new Position(2, 1))
+        await game.movePlayer2(MOVE_DIRECTIONS.LEFT)
+        position2 = await game.getPlayer2Position()
+        expect(position2).toBeEqualPosition(new Position(0, 0));
+        // [p2][  ][  ]
+        // [  ][  ][p1]
+        // [ g][  ][  ]
+        // ===========
+        await game.movePlayer1(MOVE_DIRECTIONS.UP)
+        position1 = await game.getPlayer1Position()
+        expect(position1).toBeEqualPosition(new Position(2, 0))
+        await game.movePlayer2(MOVE_DIRECTIONS.UP)
+        position2 = await game.getPlayer2Position()
+        expect(position2).toBeEqualPosition(new Position(0, 0));
+        // [p2][  ][p1]
+        // [  ][  ][  ]
+        // [ g][  ][  ]
+        // ===========
+        await game.movePlayer1(MOVE_DIRECTIONS.LEFT)
+        position1 = await game.getPlayer1Position()
+        expect(position1).toBeEqualPosition(new Position(1, 0))
+        await game.movePlayer2(MOVE_DIRECTIONS.DOWN)
+        position2 = await game.getPlayer2Position()
+        expect(position2).toBeEqualPosition(new Position(0, 1));
+        // [  ][p1][  ]
+        // [p2][  ][  ]
+        // [ g][  ][  ]
+        // ===========
+        await game.movePlayer1(MOVE_DIRECTIONS.LEFT)
+        position1 = await game.getPlayer1Position()
+        expect(position1).toBeEqualPosition(new Position(0, 0))
+        await game.movePlayer2(MOVE_DIRECTIONS.RIGHT)
+        position2 = await game.getPlayer2Position()
+        expect(position2).toBeEqualPosition(new Position(1, 1));
+        // [p1][  ][  ]
+        // [  ][p2][  ]
+        // [ g][  ][  ]
+        // ===========
+        await game.movePlayer1(MOVE_DIRECTIONS.DOWN)
+        position1 = await game.getPlayer1Position()
+        expect(position1).toBeEqualPosition(new Position(0, 1))
+        await game.movePlayer2(MOVE_DIRECTIONS.DOWN)
+        position2 = await game.getPlayer2Position()
+        expect(position2).toBeEqualPosition(new Position(1, 2));
+        // [  ][  ][  ]
+        // [p1][  ][  ]
+        // [ g][p2][  ]
+        // ===========
+    })
+
+    it("catch the Google", async () => {
+        class MockFakeNumberUtility extends NumberUtility {
+            #returnsNumbers = [
+                /*player1*/ 1, 2,
+                /*player2*/ 2, 1,
+                /*google*/ 0, 2
+            ]
+            #callsCount = 0;
+            getRandomNumber() {
+                const returnValue = this.#returnsNumbers[this.#callsCount]
+                this.#callsCount++;
+                return returnValue;
+            }
+        }
+
+        const numberUtil = new MockFakeNumberUtility()
+        const game = new Game(numberUtil);
+
+        await game.setSettings({
+            gridSize: {
+                rowsCount: 3,
+                columnsCount: 3 // x
+            }
+        })
+
+        await game.start()
+        let position1 = await game.getPlayer1Position()
+        let position2 = await game.getPlayer2Position()
+        let googlePosition = await game.getGooglePosition()
+        let catchGoogle = await game.getCheckGoogleCatching()
+        expect(position1).toBeEqualPosition(new Position(1,2))
+        expect(position2).toBeEqualPosition(new Position(2,1))
+        //expect(position2).toBeEqualPosition(new Position(2,1))
+        expect(googlePosition).toBeEqualPosition(new Position(0,2))
+        expect(catchGoogle).toBeNull()
+        // [  ][  ][  ]
+        // [  ][  ][p2]
+        // [ g][p1][  ]
+        // ===========
+        await game.movePlayer1(MOVE_DIRECTIONS.LEFT)
+        position1 = await game.getPlayer1Position()
+        expect(position1).toBeEqualPosition(new Position(0, 2));
+        await game.movePlayer2(MOVE_DIRECTIONS.LEFT)
+        position2 = await game.getPlayer2Position()
+        expect(position2).toBeEqualPosition(new Position(1, 1));
+        catchGoogle = await game.getCheckGoogleCatching()
+        expect(catchGoogle).toBe("Player 1")
+
+        await game.movePlayer1(MOVE_DIRECTIONS.RIGHT)
+        position1 = await game.getPlayer1Position()
+        expect(position1).toBeEqualPosition(new Position(1, 2));
+        await game.movePlayer2(MOVE_DIRECTIONS.LEFT)
+        position2 = await game.getPlayer2Position()
+        expect(position2).toBeEqualPosition(new Position(0, 1));
+
+        await game.movePlayer1(MOVE_DIRECTIONS.RIGHT)
+        position1 = await game.getPlayer1Position()
+        expect(position1).toBeEqualPosition(new Position(2, 2));
+        await game.movePlayer2(MOVE_DIRECTIONS.DOWN)
+        position2 = await game.getPlayer2Position()
+        expect(position2).toBeEqualPosition(new Position(0, 2));
+        catchGoogle = await game.getCheckGoogleCatching()
+        expect(catchGoogle).toBe("Player 2")
+
+    })
+
+    it("should end the game when player 1 reaches winScore", async () => {
+        class MockFakeNumberUtility extends NumberUtility {
+            #returnsNumbers = [
+                /*player1*/ 1, 2,
+                /*player2*/ 2, 1,
+                /*google*/ 0, 2
+            ]
+            #callsCount = 0;
+            getRandomNumber() {
+                const returnValue = this.#returnsNumbers[this.#callsCount]
+                this.#callsCount++;
+                return returnValue;
+            }
+        }
+
+        const numberUtil = new MockFakeNumberUtility()
+        const game = new Game(numberUtil);
+
+        await game.setSettings({
+            gridSize: {
+                rowsCount: 3,
+                columnsCount: 3 // x
+            },
+            winScore: 1
+        })
+
+        await game.start()
+        let position1 = await game.getPlayer1Position()
+        let position2 = await game.getPlayer2Position()
+        let googlePosition = await game.getGooglePosition()
+        let catchGoogle = await game.getCheckGoogleCatching()
+        expect(position1).toBeEqualPosition(new Position(1,2))
+        expect(position2).toBeEqualPosition(new Position(2,1))
+        expect(googlePosition).toBeEqualPosition(new Position(0,2))
+        expect(catchGoogle).toBeNull()
+        // [  ][  ][  ]
+        // [  ][  ][p2]
+        // [ g][p1][  ]
+        // ===========
+        await game.movePlayer1(MOVE_DIRECTIONS.LEFT)
+        position1 = await game.getPlayer1Position()
+        expect(position1).toBeEqualPosition(new Position(0, 2));
+        catchGoogle = await game.getCheckGoogleCatching()
+        expect(catchGoogle).toBe("Player 1")
+
+        let status = await game.getStatus()
+        expect(status).toBe(GAME_STATUSES.FINISHED)
+
+    })
+
+    it("should end the game when player 2 reaches winScore", async () => {
+        class MockFakeNumberUtility extends NumberUtility {
+            #returnsNumbers = [
+                /*player1*/ 1, 2,
+                /*player2*/ 0, 1,
+                /*google*/ 0, 2
+            ];
+            #callsCount = 0;
+            getRandomNumber() {
+                const returnValue = this.#returnsNumbers[this.#callsCount];
+                this.#callsCount++;
+                return returnValue;
+            }
+        }
+
+        const numberUtil = new MockFakeNumberUtility();
+        const game = new Game(numberUtil);
+
+        await game.setSettings({
+            gridSize: {
+                rowsCount: 3,
+                columnsCount: 3 // x
+            },
+            winScore: 1 // Устанавливаем winScore на 1 для быстрого завершения игры
+        });
+
+        await game.start();
+        let position1 = await game.getPlayer1Position();
+        let position2 = await game.getPlayer2Position();
+        let googlePosition = await game.getGooglePosition();
+        let catchGoogle = await game.getCheckGoogleCatching();
+        expect(position1).toBeEqualPosition(new Position(1, 2));
+        expect(position2).toBeEqualPosition(new Position(0, 1));
+        expect(googlePosition).toBeEqualPosition(new Position(0, 2));
+        expect(catchGoogle).toBeNull();
+
+        await game.movePlayer2(MOVE_DIRECTIONS.DOWN);
+        position2 = await game.getPlayer2Position();
+        expect(position2).toBeEqualPosition(new Position(0, 2));
+        catchGoogle = await game.getCheckGoogleCatching();
+        expect(catchGoogle).toBe("Player 2");
+
+        let status = await game.getStatus();
+        expect(status).toBe(GAME_STATUSES.FINISHED);
+    });
+
+    it("should end the game when Google reaches winScore", async () => {
+        class MockFakeNumberUtility extends NumberUtility {
+            #returnsNumbers = [
+                /*player1*/ 1, 2,
+                /*player2*/ 2, 1,
+                /*google*/ 0, 2
+            ];
+            #callsCount = 0;
+            getRandomNumber() {
+                const returnValue = this.#returnsNumbers[this.#callsCount];
+                this.#callsCount++;
+                return returnValue;
+            }
+        }
+
+        const numberUtil = new MockFakeNumberUtility();
+        const game = new Game(numberUtil);
+
+        await game.setSettings({
+            gridSize: {
+                rowsCount: 3,
+                columnsCount: 3 // x
+            },
+            winScore: 1 // Устанавливаем winScore на 1 для быстрого завершения игры
+        });
+
+        await game.start();
+        let position1 = await game.getPlayer1Position();
+        let position2 = await game.getPlayer2Position();
+        let googlePosition = await game.getGooglePosition();
+        let catchGoogle = await game.getCheckGoogleCatching();
+        let jumpGoogle = await game.getJumpGoogle()
+        expect(position1).toBeEqualPosition(new Position(1, 2));
+        expect(position2).toBeEqualPosition(new Position(2, 1));
+        expect(googlePosition).toBeEqualPosition(new Position(0, 2));
+        expect(catchGoogle).toBeNull();
+
+        await game.movePlayer1(MOVE_DIRECTIONS.RIGHT);
+        await game.movePlayer2(MOVE_DIRECTIONS.LEFT);
+
+        let status = await game.getStatus();
+        expect(status).toBe(GAME_STATUSES.FINISHED);
+    });
+
 })
 
 
